0beddbb47fd3de26d09bd78693b971e81bc22617
Murat Cengiz <devmuratcengiz@outlook.com>
2025-04-22 10:47:12 +0300

    MovieList api consumed and sample datasets added.

--- MovieApi.WebUI/Program.cs
+++ MovieApi.WebUI/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +24,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Movie}/{action=MovieList}/{id?}")
     .WithStaticAssets();
 
 
