cb0c0a5889b9022d9667cfb3ee9c8de173145eb5
Murat Cengiz <devmuratcengiz@outlook.com>
2025-04-30 11:38:51 +0300

    MovieDetail page created

---
+++ MovieApi.WebUI/ViewComponents/MovieDetail/_MovieDetailShareSocialMedia.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.MovieDetail
+{
+    public class _MovieDetailShareSocialMedia : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
